9eb313bfbf554edb669974641dd9e18aa772612f
Matthew Wicks <mwicks7@gmail.com>
2023-04-06 15:15:29 -0400

    Update bio and skills

--- src/About.js
+++ src/About.js
@@ -6,9 +6,9 @@ const About = () => {
       <img className="About-image" height="155" width="155" alt="Matthew Wicks" src={imageProfile} />
 
       <div className="About-details">
-        <h1>&#9996; Heyyo World, I'm Matthew Wicks</h1>
+        <h1>&#9996; Hey-yo World, I'm Matthew Wicks</h1>
         <p>
-          I am a Philly-based software engineer with 13 years experience and a keen interest in web standards, accessibility, and building fast, pixel-perfect interfaces. 8 years in the ecommerce industry has provided me with vast knowledge to develop the integral components that make up the online shopping experience.
+          A Philly-based software engineer with 15 years of development experience, a passion for learning new technologies, and a keen interest in web standards and building fast pixel-perfect interfaces. 8 years in the Ecommerce industry has provided me with vast knowledge to develop the integral components that make up the online shopping experience.
         </p>
       </div>
     </div>
--- src/Skills.js
+++ src/Skills.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
-const frontEndSkills = ['HTML', 'SCSS', 'Javascript', 'a11y', 'SEO', 'Analytics', 'Node', 'React', 'jQuery']
-const backEndSkills = ['Ruby on Rails', 'Php', 'MySQL', 'MongoDB', 'CodeIgniter', 'Wordpress']
-const ecommerceSkills = ['Workarea', 'Shopify']
+const frontEndSkills = ['HTML/Haml', 'CSS/SASS/Responsive Design', 'Javascript/React/jQuery', 'a11y', 'SEO', 'Analytics']
+const backEndSkills = ['Rails/MongoDB', 'Apache/MySQL/PHP', 'Node/Express']
+const frameworkSkills = ['Ruby on Rails', 'Workarea Ecommerce', 'Shopify', 'CodeIgniter', 'Wordpress', 'Tumblr', 'Facebook Apps']
+const cliSkills = ['Git', 'SSH', 'Homebrew']
+const designSkills = ['Adobe Photoshop & Illustrator']
 
 const Skills = () => {
   return (
     <div className="Skills">
-        <h2>Skills</h2>
-        <div><b>Front-End:</b> {frontEndSkills.join(", ")}</div>
-        <div><b>Back-End:</b> {backEndSkills.join(", ")}</div>
-        <div><b>Ecommerce:</b> {ecommerceSkills.join(", ")}</div>
+        <h2>Skills Experience</h2>
+        <p><b>Front-End:</b> {frontEndSkills.join(" · ")}</p>
+        <p><b>Back-End:</b> {backEndSkills.join(" · ")}</p>
+        <p><b>Frameworks:</b> {frameworkSkills.join(" · ")}</p>
+        <p><b>Command Line:</b> {cliSkills.join(" · ")}</p>
+        <p><b>Design:</b> {designSkills.join(" · ")}</p>
     </div>
   )
 }
