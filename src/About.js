312d330a58f67b75a95c6f3796a5889aa0cd6fa5
Matthew Wicks <mwicks7@gmail.com>
2023-04-10 11:53:57 -0400

    Update about copy

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ const About = () => {
       <div className="About-details">
         <h1>&#9996; Hey-yo World, I'm Matthew Wicks</h1>
         <p>
-          A Philly-based software engineer with 15 years of development experience, a passion for learning new technologies, and a keen interest in web standards and building fast pixel-perfect interfaces. 9 years in the Ecommerce industry has provided me with vast knowledge to develop the integral components that make up the online shopping experience.
+          A Philly-based software engineer with 15 years of development experience, a passion for learning new technologies, a keen interest in web standards, and drive for building fast pixel-perfect interfaces. 9 years in the Ecommerce industry has provided me with vast knowledge to develop the integral components that make up the online shopping experience.
         </p>
       </div>
     </div>
