5210cf044e3077055a00090030626a76247e983c
Matthew Wicks <mwicks7@gmail.com>
2023-04-17 07:29:02 -0400

    Update skills

--- src/Skills.js
+++ src/Skills.js
@@ -3,7 +3,7 @@ import React from 'react'
 const frontEndSkills = ['HTML/Haml', 'CSS/SASS/Responsive Design', 'Javascript/React/jQuery', 'a11y', 'SEO', 'Analytics']
 const backEndSkills = ['Rails/MongoDB', 'Apache/MySQL/PHP', 'Node/Express']
 const frameworkSkills = ['Ruby on Rails', 'Workarea Ecommerce', 'Shopify', 'CodeIgniter', 'Wordpress', 'Tumblr', 'Facebook Apps']
-const cliSkills = ['Git', 'SSH', 'Homebrew']
+const cliSkills = ['Git', 'SSH', 'Homebrew', 'npm']
 const designSkills = ['Adobe Photoshop & Illustrator']
 
 const Skills = () => {
