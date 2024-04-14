73dc3ea78898b4af9d1abdd515b2474b9da2749f
Matthew Wicks <mwicks7@gmail.com>
2024-04-14 12:37:48 -0400

    Update skills

--- src/Skills.js
+++ src/Skills.js
@@ -1,22 +1,49 @@
-import React from 'react'
+import React from "react";
 
-const frontEndSkills = ['HTML/Haml', 'CSS/SASS/Responsive Design', 'Javascript/React/jQuery', 'a11y', 'SEO', 'Analytics']
-const backEndSkills = ['Rails/MongoDB', 'Apache/MySQL/PHP', 'Node/Express']
-const frameworkSkills = ['Ruby on Rails', 'Workarea Ecommerce', 'Shopify', 'CodeIgniter', 'Wordpress', 'Tumblr', 'Facebook Apps']
-const cliSkills = ['Git', 'SSH', 'Homebrew', 'npm']
-const designSkills = ['Adobe Photoshop & Illustrator']
+const frontEndSkills = [
+  "TypeScript/React/jQuery",
+  "HTML/Haml",
+  "CSS/SASS/Responsive Design",
+  "a11y",
+  "SEO",
+  "Analytics",
+];
+const backEndSkills = ["GraphQL", "Rails/MongoDB", "Apache/MySQL/PHP"];
+const frameworkSkills = [
+  "Next.js",
+  "VTEX Faststore",
+  "Workarea Ecommerce",
+  "Shopify",
+  "Ruby on Rails",
+  "CodeIgniter",
+  "Wordpress",
+  "Tumblr",
+  "Facebook Apps",
+];
+const cliSkills = ["Git", "SSH", "Homebrew", "npm"];
+const designSkills = ["Figma", "Adobe Photoshop & Illustrator"];
 
 const Skills = () => {
   return (
     <div className="Skills">
-        <h2>Skills Experience</h2>
-        <p><b>Front-End:</b> {frontEndSkills.join(" · ")}</p>
-        <p><b>Back-End:</b> {backEndSkills.join(" · ")}</p>
-        <p><b>Frameworks:</b> {frameworkSkills.join(" · ")}</p>
-        <p><b>Command Line:</b> {cliSkills.join(" · ")}</p>
-        <p><b>Design:</b> {designSkills.join(" · ")}</p>
+      <h2>Skills Experience</h2>
+      <p>
+        <b>Front-End:</b> {frontEndSkills.join(" · ")}
+      </p>
+      <p>
+        <b>Back-End:</b> {backEndSkills.join(" · ")}
+      </p>
+      <p>
+        <b>Frameworks:</b> {frameworkSkills.join(" · ")}
+      </p>
+      <p>
+        <b>Command Line:</b> {cliSkills.join(" · ")}
+      </p>
+      <p>
+        <b>Design:</b> {designSkills.join(" · ")}
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default Skills;
