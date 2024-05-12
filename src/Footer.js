b5dc83e62c898f4bcd0b1bd7f9567f01bac69f7f
Matthew Wicks <mwicks7@gmail.com>
2024-05-12 09:56:51 -0400

    Update copy

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react"
 
 const Footer = () => {
   return (
-    <div className='Footer'>
-      <h2>&#128075; Thanks for stopping by. Check me out on <a href="https://www.linkedin.com/in/mwicks/">LinkedIn</a> &amp; <a href="https://github.com/mwicks7/">github</a></h2>
+    <div className="Footer">
+      <h2>
+        &#128075; Thanks for stopping by. Check me out on{" "}
+        <a href="https://www.linkedin.com/in/mwicks/">LinkedIn</a> &amp;{" "}
+        <a href="https://github.com/mwicks7/">GitHub</a>
+      </h2>
     </div>
   )
 }
--- src/Lab.js
+++ src/Lab.js
@@ -1,62 +1,48 @@
-import fifthsImage from "./images/lab_table_of_fifths7.webp";
-import sedsoImage from "./images/sedso_content_manager.jpg";
-import jeopardyImage from "./images/lab_jeopardy.jpg";
-import rockImage from "./images/lab_rock_paper_scissors.jpg";
-import parksImage from "./images/lab_national_parks.jpg";
-import astroImage from "./images/lab_astro.jpg";
+import fifthsImage from "./images/lab_table_of_fifths7.webp"
+import sedsoImage from "./images/sedso_content_manager.jpg"
+import jeopardyImage from "./images/lab_jeopardy.jpg"
+import rockImage from "./images/lab_rock_paper_scissors.jpg"
+import parksImage from "./images/lab_national_parks.jpg"
+import astroImage from "./images/lab_astro.jpg"
 
 const projectData = [
   {
-    img: astroImage,
-    title: "Astro Ecommerce",
-    description:
-      "Want to own a piece of the cosmos? Now you can with this faux ecommerce site. (in progress)",
-    madeWith: "Next.js, MongoDB",
-    href: "https://e-commerce-lab.vercel.app/",
-    links: [
-      {
-        text: "Launch App",
-        href: "https://e-commerce-lab.vercel.app/",
-      },
-      {
-        text: "View source",
-        href: "https://github.com/mwicks7/e-commerce-lab",
-      },
-    ],
-  },
-  {
     img: parksImage,
-    title: "US National Parks Map Directory",
+    title: "USA National Parks Map Directory",
     description:
-      "Explore what to do and where to go when visiting the national parks in America.",
-    madeWith: "Next.js, MongoDB, nps.gov API, and Google Maps API",
+      "Explore where to go and what to do when visiting the country's national parks.",
+    madeWith: [
+      "Next.js",
+      "MongoDB",
+      "National Park Service API",
+      "Google Maps API",
+    ],
     href: "https://national-parks-directory.vercel.app/",
     links: [
       {
-        text: "Launch App",
+        text: "Explore",
         href: "https://national-parks-directory.vercel.app/",
       },
       {
-        text: "View source",
+        text: "GitHub",
         href: "https://github.com/mwicks7/national-parks-directory",
       },
     ],
   },
   {
-    img: jeopardyImage,
-    title: "JEOPARDY!js",
-    description:
-      'I\'m not Alex Trebek, but, "THIS IS JEOPARDY!" Test your trivia skills with actual clues from 38 seasons of the show.',
-    madeWith: "React and JService.io",
-    href: "https://mwicks7.github.io/jeopardy.js",
+    img: astroImage,
+    title: "Astro Ecommerce",
+    description: "Want to own a piece of the cosmos?",
+    madeWith: ["Next.js", "MongoDB"],
+    href: "https://e-commerce-lab.vercel.app/",
     links: [
       {
-        text: "Launch App",
-        href: "https://mwicks7.github.io/jeopardy.js",
+        text: "Shop",
+        href: "https://e-commerce-lab.vercel.app/",
       },
       {
-        text: "View source",
-        href: "https://github.com/mwicks7/jeopardy.js",
+        text: "GitHub",
+        href: "https://github.com/mwicks7/e-commerce-lab",
       },
     ],
   },
@@ -64,17 +50,34 @@ const projectData = [
     img: fifthsImage,
     title: "The Table of Fifths",
     description:
-      "You might be familiar with <i>The Circle of Fifths</i>. Well, <i>The Table of Fifths</i> deconstructs the wheel and utilizes instrument diagrams with music notation to help you learn fundamental key signatures and the relationships between them.",
-    madeWith: "React and tonal.js",
+      "Learn fundamental musical key signatures through notation and instrument diagrams.",
+    madeWith: ["React", "tonal.js"],
     href: "https://mwicks7.github.io/the-table-of-fifths",
     links: [
       {
-        text: "Launch App",
+        text: "Learn",
         href: "https://mwicks7.github.io/the-table-of-fifths",
       },
       {
-        text: "View source",
+        text: "GitHub",
         href: "https://github.com/mwicks7/the-table-of-fifths",
+      },
+    ],
+  },
+  {
+    img: jeopardyImage,
+    title: "JEOPARDY!js",
+    description: "Test your trivia with clues from the legendary game show.",
+    madeWith: ["React", "JService.io"],
+    href: "https://mwicks7.github.io/jeopardy.js",
+    links: [
+      {
+        text: "Play",
+        href: "https://mwicks7.github.io/jeopardy.js",
+      },
+      {
+        text: "GitHub",
+        href: "https://github.com/mwicks7/jeopardy.js",
       },
     ],
   },
@@ -95,20 +98,20 @@ const projectData = [
     title: "Rock, Paper, Scissors: Humans vs Robots",
     description:
       "Do you have what it takes to beat the robots in the classic game <i>Rock, Paper, Scissors</i>?",
-    madeWith: "React",
+    madeWith: ["React"],
     href: "https://mwicks7.github.io/rock-paper-scissors",
     links: [
       {
-        text: "Launch App",
+        text: "Play",
         href: "https://mwicks7.github.io/rock-paper-scissors",
       },
       {
-        text: "View source",
+        text: "GitHub",
         href: "https://github.com/mwicks7/rock-paper-scissors",
       },
     ],
   },
-];
+]
 
 const Article = ({ project }) => {
   return (
@@ -127,7 +130,11 @@ const Article = ({ project }) => {
         <p dangerouslySetInnerHTML={{ __html: project.description }} />
 
         <p>
-          <b>Made with:</b> {project.madeWith}
+          <i>
+            {project.madeWith
+              .map((skill) => <span className="Lab-skill">{skill}</span>)
+              .reduce((prev, curr) => [prev, " · ", curr])}
+          </i>
         </p>
 
         <p className="Lab-links">
@@ -146,8 +153,8 @@ const Article = ({ project }) => {
         </p>
       </div>
     </article>
-  );
-};
+  )
+}
 
 const Lab = () => {
   return (
@@ -159,7 +166,7 @@ const Lab = () => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Lab;
+export default Lab
